80a6d7ffa9b3dd70c4bc57edde1cb739bdd4a8d4
BuildTools <unconfigured@null.spigotmc.org>
2024-08-07 07:51:47 +0900

    레이아웃 재정리

--- script/script.js
+++ script/script.js
@@ -73,40 +73,45 @@ $(document).ready(function() {
 
     $(function() {
         const mainProjectCate = [
-            { id: 0, title: "[머신명] 내 마음대로 원하는 커피를 추출하는 신개념 커피머신", category: "커피"},
-            { id: 1, title: "[향초명] 버튼만 누르면 알아서 켜지는 스마트 향초", category: "향초"},
-            { id: 2, title: "[기계명] 힘든 칼질 필요없이 원터치 다지기 기계", category: "식품"},
-            { id: 3, title: "[도구명] 쓱쓱 빗으면 털들이 싹", category: "반려동물"},
-            { id: 4, title: "[게임명] 조선의 미스테리 사건을 밝히는 추리 게임", category: "디지털 콘텐츠"},
-            { id: 5, title: "[제품명] ", category: "헬스케어"},
+            { num: 0, title: "[머신명] 내 마음대로 원하는 커피를 추출하는 신개념 커피머신", category: "커피", icon: "fa-solid fa-mug-hot"},
+            { num: 1, title: "[향초명] 버튼만 누르면 알아서 켜지는 스마트 향초", category: "향초", icon: "fa-solid fa-fire-flame-simple"},
+            { num: 2, title: "[기계명] 힘든 칼질 필요없이 원터치 다지기 기계", category: "식품", icon: "fa-solid fa-drumstick-bite"},
+            { num: 3, title: "[도구명] 쓱쓱 빗으면 털들이 싹!", category: "반려 동물", icon: "fa-solid fa-paw"},
+            { num: 4, title: "[게임명] 조선의 미스테리 사건을 밝히는 추리 게임", category: "디지털 콘텐츠", icon: "fa-solid fa-dice"},
+            { num: 5, title: "[제품명] ", category: "헬스케어", icon: "fa-solid fa-heart"},
         ];
 
         mainProjectCate.forEach(function (data) {
+            const title = ` `;
             const template = `
-                <a class="project-cate-wrap" href="/projects">
-                    <div class="project-cate-thumb"> <!-- 이미지 썸네일 영역 -->
-                        <img src="https://picsum.photos/400/400?random=${data.id}">
+                <div class="project-cate-wrap">
+                    <div class="project-subject">
+                        <p class="project-subject-title font16"><i class="${data.icon} font22"></i>${data.category}</p>
+                        <a class="project-subject-more font16" href="#">더 보기</p>
                     </div>
-                    <p class="project-cate desc font14"> <!-- 프로젝트 요약 설명 영역 -->
-                        ${data.category} 관련 프로젝트!
-                    </p>
-                    <p class="project-cate title font16"> <!-- 프로젝트 제목 영역 -->
-                        ${data.title}
-                    </p>
-                    <div class="project-cate rate">
-                        <p class="project-cate-rate percent font14"> <!-- 달성 % 표시 영역 -->
-                            <b>100%</b> 달성
+                    <a class="project-cate-thumb-wrap" href="/projects">
+                        <div class="project-cate-thumb"> <!-- 이미지 썸네일 영역 -->
+                            <img src="https://picsum.photos/400/400?random=${data.num}">
+                        </div>
+                        <p class="project-cate title font16"> <!-- 프로젝트 제목 영역 -->
+                            ${data.title}
                         </p>
-                        <p class="project-cate-rate star font14"> <!-- 달성 % 표시 영역 -->
-                            ★ 4.0
+                        <div class="project-cate rate">
+                            <p class="project-cate-rate percent font14"> <!-- 달성 % 표시 영역 -->
+                                <b>100%</b> 달성
+                            </p>
+                            <p class="project-cate-rate star font14"> <!-- 달성 % 표시 영역 -->
+                                ★ 4.0
+                            </p>
+                        </div>
+                        <p class="project-cate name font14"> <!-- 창작자/단체명 영역 -->
+                            창작자
                         </p>
-                    </div>
-                    <p class="project-cate name font14"> <!-- 창작자/단체명 영역 -->
-                        창작자
-                    </p>
-                </a>
+                    </a>
+                </div>
             `;
 
+            $(".project-subject-title").append(title);
             $(".main-cate-project").append(template);
         });
     });
@@ -207,6 +212,24 @@ $(document).ready(function() {
             $(".trend-project-block").append(template);
         });
     });
+
+
+    // ----------------------------------------------------------------
+    //           트렌드 프로젝트 버튼 눌렀을때 가로 스크롤 이동
+    // ----------------------------------------------------------------
+    
+    $(function () {
+        $('.trend-project-btn').click(function() {
+            const btnEl = $(this).siblings('.trend-project-block');
+            let curScroll = $(btnEl).scrollLeft();
+
+            if ($(this).attr('class') === 'trend-project-btn right') {
+                $(btnEl).animate({scrollLeft : curScroll+1000}, 400);
+            } else {
+                $(btnEl).animate({scrollLeft : curScroll-1000}, 400);
+            }
+        })
+    })
 
 
     // ----------------------------------------------------------------
