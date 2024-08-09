5103259b84e0db72ba292f50b6bd5631fc8cc00b
kinari4041 <1gml2tjd@naver.com>
2024-08-09 18:28:41 +0900

    차후 데이터 연동 준비를 위해 샘플 데이터를 json 파일 형식으로 분리, 주제별 추천 프로젝트 섹션의 주제별 랜덤 프로젝트 출력 기능 구현

--- script/script.js
+++ script/script.js
@@ -1,94 +1,6 @@
 $(document).ready(function() {
-    
-    // ----------------------------------------------------------------
-    //                      테스트용 임시 데이터
-    // ----------------------------------------------------------------
-    
-    // 프로젝트 목록 데이터
-    const projectData = [
-        { rank: 1, id: 1, name: "제품1", title: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.", rate: "5.0", like: "1234", achive: "100", author: "창작자1"},
-        { rank: 2, id: 2, name: "제품2", title: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.", rate: "4.2", like: "1234", achive: "1200", author: "창작자2"},
-        { rank: 3, id: 3, name: "제품3", title: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.", rate: "5.0", like: "1234", achive: "300", author: "창작자3"},
-        { rank: 4, id: 4, name: "제품4", title: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.", rate: "4.9", like: "1234", achive: "420", author: "창작자4"},
-        { rank: 5, id: 5, name: "제품5", title: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.", rate: "4.3", like: "1234", achive: "1200", author: "창작자5"},
-        { rank: 6, id: 6, name: "제품6", title: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.", rate: "4.8", like: "1234", achive: "12000", author: "창작자6"},
-        { rank: 7, id: 7, name: "제품7", title: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.", rate: "4.2", like: "1234", achive: "1600", author: "창작자7"},
-        { rank: 8, id: 8, name: "제품8", title: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.", rate: "4.9", like: "1234", achive: "7000", author: "창작자8"},
-        { rank: 9, id: 9, name: "제품9", title: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.", rate: "3.9", like: "1234", achive: "4500", author: "창작자9"},
-        { rank: 10, id: 10, name: "제품10", title: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.", rate: "4.0", like: "1234", achive: "1523", author: "창작자10"},
-    ];
 
-    // 카테고리 세부 아이템 데이터
-    const cateItems = [
-        { id:'반려 동물', title: "사료"},
-        { id:'반려 동물', title: "간식"},
-        { id:'반려 동물', title: "놀이기구"},
-        { id:'반려 동물', title: "관리용품"},
-        { id:'반려 동물', title: "고양이 모래"},
-        { id:'반려 동물', title: "의류"},
-        { id:'반려 동물', title: "파충류"},
-        { id:'반려 동물', title: "어류"},
-        { id:'반려 동물', title: "기타"},
-        { id:'헬스 케어', title: "건강식품"},
-        { id:'헬스 케어', title: "헬스 기구"},
-        { id:'헬스 케어', title: "보충제"},
-        { id:'헬스 케어', title: "영양제"},
-        { id:'헬스 케어', title: "건강식품"},
-        { id:'헬스 케어', title: "스포츠 웨어"},
-        { id:'디지털 미디어', title: "도서"},
-        { id:'디지털 미디어', title: "음악"},
-        { id:'디지털 미디어', title: "게임"},
-        { id:'디지털 미디어', title: "웹툰"},
-        { id:'디지털 미디어', title: "클래스"},
-        { id:'디지털 미디어', title: "아트"},
-        { id:'향초', title: "캔들"},
-        { id:'향초', title: "캔들워머"},
-        { id:'향초', title: "방향제"},
-        { id:'향초', title: "향수/공병"},
-        { id:'향초', title: "인센스"},
-        { id:'향초', title: "차량용 방향제"},
-        { id:'향초', title: "디퓨저"},
-        { id:'향초', title: "오일"},
-        { id:'커피', title: "원두"},
-        { id:'커피', title: "콜드브루"},
-        { id:'커피', title: "커피용품"},
-        { id:'커피', title: "머신"},
-        { id:'커피', title: "캡슐"},
-        { id:'커피', title: "드립커피"},
-        { id:'커피', title: "디카페인"},
-        { id:'식품', title: "냉동식품"},
-        { id:'식품', title: "안주류"},
-        { id:'식품', title: "고기류/육류"},
-        { id:'식품', title: "빵/간식"},
-        { id:'식품', title: "비건"},
-        { id:'식품', title: "음료"},
-        { id:'식품', title: "해산물"},
-        { id:'식품', title: "와인"},
-        { id:'식품', title: "전통주"},
-        { id:'식품', title: "요리책"},
-        { id:'식품', title: "푸드 페스티벌"},
-        { id:'홈/리빙', title: "침실"},
-        { id:'홈/리빙', title: "욕실"},
-        { id:'홈/리빙', title: "주방"},
-        { id:'홈/리빙', title: "화훼/원예"},
-        { id:'홈/리빙', title: "청소/세탁"},
-        { id:'홈/리빙', title: "인테리어"},
-        { id:'홈/리빙', title: "DIY"},
-        { id:'패션', title: "주얼리"},
-        { id:'패션', title: "의류"},
-        { id:'패션', title: "가방"},
-        { id:'패션', title: "신발"},
-        { id:'패션', title: "언더웨어"},
-        { id:'패션', title: "패션소품"},
-        { id:'패션', title: "한복"},
-        { id:'패션', title: "홈웨어"},
-        { id:'패션', title: "키즈"},
-        { id:'문구', title: "캘린더"},
-        { id:'문구', title: "다이어리"},
-        { id:'문구', title: "스티커"},
-        { id:'문구', title: "파우치"}
-    ];
-
+    
     // ----------------------------------------------------------------
     //                              검색 팝업
     // ----------------------------------------------------------------
@@ -170,33 +82,30 @@ $(document).ready(function() {
 
 
     // ----------------------------------------------------------------
-    //                     주목할 만한 프로젝트 목록
+    //                    주제별 추천 프로젝트 목록
     // ----------------------------------------------------------------
 
     $(function() {
-        const mainProjectCate = [
-            { num: 0, title: "[머신명] 내 마음대로 원하는 커피를 추출하는 신개념 커피머신", category: "커피", icon: "fa-solid fa-mug-hot"},
-            { num: 1, title: "[향초명] 버튼만 누르면 알아서 켜지는 스마트 향초", category: "향초", icon: "fa-solid fa-fire-flame-simple"},
-            { num: 2, title: "[기계명] 힘든 칼질 필요없이 원터치 다지기 기계", category: "식품", icon: "fa-solid fa-drumstick-bite"},
-            { num: 3, title: "[도구명] 쓱쓱 빗으면 털들이 싹!", category: "반려 동물", icon: "fa-solid fa-paw"},
-            { num: 4, title: "[게임명] 조선의 미스테리 사건을 밝히는 추리 게임", category: "디지털 콘텐츠", icon: "fa-solid fa-dice"},
-            { num: 5, title: "[제품명] ", category: "헬스케어", icon: "fa-solid fa-heart"},
-        ];
+        const cateWraps = $("[class^=project-cate-wrap]");
+        const maxCount = 7;
+        let count = 0;
+        projectData.forEach(function (data) {
+            if ($(cateWraps[count]).data("category") === `${data.category}`) {
 
-        mainProjectCate.forEach(function (data) {
-            const title = ` `;
-            const template = `
-                <div class="project-cate-wrap">
+                const tempData = projectData.filter(data => data.category === $(cateWraps[count]).data("category"));
+                const index = (Math.floor(Math.random()*tempData.length));
+                
+                const template = `
                     <div class="project-subject">
-                        <p class="project-subject-title"><i class="${data.icon}"></i>${data.category}</p>
+                        <p class="project-subject-title"><i class="${tempData[index].icon}"></i>${tempData[index].category}</p>
                         <a class="project-subject-more" href="#">더 보기</p>
                     </div>
                     <a class="project-cate-thumb-wrap" href="/projects">
                         <div class="project-cate-thumb"> <!-- 이미지 썸네일 영역 -->
-                            <img src="https://picsum.photos/400/400?random=${data.num}">
+                            <img src="https://picsum.photos/400/400?random=${tempData[index].id}">
                         </div>
                         <p class="project-cate title"> <!-- 프로젝트 제목 영역 -->
-                            ${data.title}
+                            ${tempData[index].title}
                         </p>
                         <div class="project-cate rate">
                             <p class="project-cate-rate percent font14"> <!-- 달성 % 표시 영역 -->
@@ -207,14 +116,15 @@ $(document).ready(function() {
                             </p>
                         </div>
                         <p class="project-cate name font14"> <!-- 창작자/단체명 영역 -->
-                            창작자
+                            ${tempData[index].author}
                         </p>
                     </a>
-                </div>
-            `;
-
-            $(".project-subject-title").append(title);
-            $(".main-cate-project").append(template);
+                `;
+                if (count < maxCount) {
+                    $(cateWraps[count]).append(template);
+                    count++;
+                } 
+            }
         });
     });
 
@@ -224,6 +134,8 @@ $(document).ready(function() {
     // ----------------------------------------------------------------
     
     $(() => {
+        const maxCount = 10;
+        let count = 0;
         // 차후 데이터는 쿼리문의 select * from where section = "${section}" 과 같은 형식으로 불러옴
         projectData.forEach(function (data) {
             const template = `
@@ -237,8 +149,11 @@ $(document).ready(function() {
                     <p class="project-cate name">${data.author}</p>
                 </div>
                 `;
-            $(".list-page").append(template);
-            $(".trend-project-list").append(template);
+            if (count < maxCount) {
+                $(".list-page").append(template);
+                $(".trend-project-list").append(template);
+                count++
+            }
         });
     });
 
@@ -279,26 +194,31 @@ $(document).ready(function() {
     // ----------------------------------------------------------------
     
     $(() => {
+        const maxCount = 10;
+        let count = 0;
         // 차후 데이터는 쿼리문의 select * from where section = "${section}" 과 같은 형식으로 불러옴
-        projectData.forEach(function (data) {
-            const template = `
-                <li>
-                    <a href="">
-                        <div class="rank-items">
-                            <div class="rank-items__front">
-                                <p class="rank-number">${data.rank}</p>
-                                <p class="rank-title">${data.title}</p>
+            projectData.forEach(function (data) {
+                const template = `
+                    <li>
+                        <a href="">
+                            <div class="rank-items">
+                                <div class="rank-items__front">
+                                    <p class="rank-number">${data.rank}</p>
+                                    <p class="rank-title">${data.title}</p>
+                                </div>
+                                <div class="rank-items__back">
+                                    <p class="rank-number">${data.rank}</p>
+                                    <p class="rank-title">${data.author}</p>
+                                </div>
                             </div>
-                            <div class="rank-items__back">
-                                <p class="rank-number">${data.rank}</p>
-                                <p class="rank-title">${data.author}</p>
-                            </div>
-                        </div>
-                    </a>
-                </li>
-                `;
-            $(".rank-wrap").append(template);
-        });
+                        </a>
+                    </li>
+                    `;
+                    if (count < maxCount) {
+                        $(".rank-wrap").append(template);
+                        count++;
+                    } 
+            });
     });
 
     
